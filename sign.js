7ba9a7c90fc0843e99485d2a05c9c1172249df43
Ganesh-S-Prabhu <ganeshsprabhu2000@gmail.com>
2022-01-20 18:07:38 +0530

    signin and create Account

---
+++ sign.js
@@ -0,0 +1,25 @@
+var createArr=JSON.parse(localStorage.getItem("createItem"));
+     var c=0;
+     document.querySelector("form").addEventListener("submit",getIn);
+    function getIn(event){
+        
+        event.preventDefault();
+        var email=document.querySelector("#emil").value;
+        var pwd=document.querySelector("#password").value;
+        
+        for(i=0;i<createArr.length;i++)
+        { var c=0;
+            if(createArr[i].email==email && createArr[i].pass==pwd)
+            {
+                c=1;
+                break;
+            }
+        }
+        if(c==1){
+            alert("Login Successfully");
+            window.location.href="homepage.html";
+        }
+        else{
+            alert("Email or Password is incorrect");
+        }
+    }
