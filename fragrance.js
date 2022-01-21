ff564799ad702157d7eeb842af7390e8741dbfdf
Pranav Void <pranavvoid@pm.me>
2022-01-21 15:25:38 +0530

    multiple pages

---
+++ fragrance.js
@@ -0,0 +1,110 @@
+var basketItem=JSON.parse(localStorage.getItem("basketItem"))||[];
+var sephoraItemsData=JSON.parse(localStorage.getItem("sephoraItemsData"));
+var fragranceArr=sephoraItemsData.filter(function(ele){
+    return ele.category=="fragrance";
+})
+createDisplay(fragranceArr);
+
+ document.querySelector("#all").addEventListener("click",function(){
+ createDisplay(fragranceArr);
+});
+
+document.querySelector("#women").addEventListener("click",women);
+function women(){
+   var womenArr=fragranceArr.filter(function(ele){
+       return ele.type=="women";
+   })
+   createDisplay(womenArr);
+}
+document.querySelector("#men").addEventListener("click",men);
+function men(){
+   var menArr=fragranceArr.filter(function(ele){
+       return ele.type=="men";
+   })
+   createDisplay(menArr);
+}
+document.querySelector("#candles").addEventListener("click",candles);
+function candles(){
+   var candlesArr=fragranceArr.filter(function(ele){
+       return ele.type=="candles";
+   })
+   createDisplay(candlesArr);
+}
+
+function createDisplay(arr){
+ document.querySelector("#items").textContent="";
+ document.querySelector("#total").innerHTML=arr.length+"  Results Found";
+    arr.map(function(ele){
+     var img1=document.createElement("img");
+    var head1=document.createElement("p");
+    var p1=document.createElement("h4");
+    var head2=document.createElement("h4");
+    var p2=document.createElement("p");
+    var head3=document.createElement("h3");
+    var div1=document.createElement("div");
+    div1.setAttribute("id","rate");
+    head1.setAttribute("id","dis");
+    var div2=document.createElement("div");
+    var btn=document.createElement("button");
+    btn.setAttribute("id","addToCart");
+    img1.setAttribute("id","images");
+    img1.setAttribute("src",ele.imageUrl);
+    head1.textContent=ele.name;
+    
+    p1.textContent=ele.brand;
+
+    head2.innerHTML=ele.stars+'<i class="fas fa-star"></i>'+"   "+ele.numReviews+"   (Reviews)";
+   
+    head3.textContent="Price is:  $"+ele.price;
+    head3.setAttribute("id","price");
+    btn.textContent="Add to Basket ";
+   
+    btn.addEventListener("click",function(){
+        addItem(ele);
+    });
+    head2.style.margin="20px";
+    div1.append(head2);
+    div2.append(img1,p1,head1,div1,head3,btn);
+    div2.style.textAlign="center";
+     div2.style.height="400px"
+
+    document.querySelector("#items").append(div2); 
+    })
+ }
+
+ function addItem(ele){
+     basketItem.push(ele);
+     alert("Successfully Added to the Basket");
+     localStorage.setItem("basketItem",JSON.stringify(basketItem));
+ }
+    
+
+
+ function sorting(){
+      var selected=document.querySelector("#selectSort").value;
+      if(selected=="")
+      {
+         createDisplay(fragranceArr);
+      }
+      else{
+          
+      if(selected=="low"){
+       var sortArr=fragranceArr.sort(function(a,b){
+          return a.price-b.price;
+      })
+      }
+      if(selected=="high"){
+         var sortArr=fragranceArr.sort(function(a,b){
+          return b.price-a.price;
+      })
+      }
+      if(selected=="rated"){
+         var sortArr=fragranceArr.sort(function(a,b){
+          return b.numReviews-a.numReviews;
+      })
+     }
+     
+      createDisplay(sortArr);
+      
+  }
+ }
---
+++ tools.js
@@ -0,0 +1,109 @@
+var basketItem=JSON.parse(localStorage.getItem("basketItem"))||[];
+var sephoraItemsData=JSON.parse(localStorage.getItem("sephoraItemsData"));
+var toolsArr=sephoraItemsData.filter(function(ele){
+    return ele.category=="tools";
+})
+createDisplay(toolsArr);
+
+ document.querySelector("#all").addEventListener("click",function(){
+ createDisplay(toolsArr);
+});
+document.querySelector("#hair").addEventListener("click",hair);
+function hair(){
+   var hairArr=toolsArr.filter(function(ele){
+       return ele.type=="hair";
+   })
+   createDisplay(hairArr);
+}
+document.querySelector("#beauty").addEventListener("click",beauty);
+function beauty(){
+   var beautyArr=toolsArr.filter(function(ele){
+       return ele.type=="beauty";
+   })
+   createDisplay(beautyArr);
+}
+document.querySelector("#travelcases").addEventListener("click",travelcases);
+function travelcases(){
+   var travelcasesArr=toolsArr.filter(function(ele){
+       return ele.type=="travel cases";
+   })
+   createDisplay(travelcasesArr);
+}
+
+function createDisplay(arr){
+ document.querySelector("#items").textContent="";
+ document.querySelector("#total").innerHTML=arr.length+"  Results Found";
+    arr.map(function(ele){
+     var img1=document.createElement("img");
+    var head1=document.createElement("p");
+    var p1=document.createElement("h4");
+    var head2=document.createElement("h4");
+    var p2=document.createElement("p");
+    var head3=document.createElement("h3");
+    var div1=document.createElement("div");
+    div1.setAttribute("id","rate");
+    head1.setAttribute("id","dis");
+    var div2=document.createElement("div");
+    var btn=document.createElement("button");
+    btn.setAttribute("id","addToCart");
+    img1.setAttribute("id","images");
+    img1.setAttribute("src",ele.imageUrl);
+    head1.textContent=ele.name;
+    
+    p1.textContent=ele.brand;
+
+    head2.innerHTML=ele.stars+'<i class="fas fa-star"></i>'+"   "+ele.numReviews+"   (Reviews)";
+   
+    head3.textContent="Price is:  $"+ele.price;
+    head3.setAttribute("id","price");
+    btn.textContent="Add to Basket ";
+   
+    btn.addEventListener("click",function(){
+        addItem(ele);
+    });
+    head2.style.margin="20px";
+    div1.append(head2);
+    div2.append(img1,p1,head1,div1,head3,btn);
+    div2.style.textAlign="center";
+     div2.style.height="400px"
+
+    document.querySelector("#items").append(div2); 
+    })
+ }
+
+ function addItem(ele){
+     basketItem.push(ele);
+     alert("Successfully Added to the Basket");
+     localStorage.setItem("basketItem",JSON.stringify(basketItem));
+ }
+    
+
+
+ function sorting(){
+      var selected=document.querySelector("#selectSort").value;
+      if(selected=="")
+      {
+         createDisplay(toolsArr);
+      }
+      else{
+          
+      if(selected=="low"){
+       var sortArr=toolsArr.sort(function(a,b){
+          return a.price-b.price;
+      })
+      }
+      if(selected=="high"){
+         var sortArr=toolsArr.sort(function(a,b){
+          return b.price-a.price;
+      })
+      }
+      if(selected=="rated"){
+         var sortArr=toolsArr.sort(function(a,b){
+          return b.numReviews-a.numReviews;
+      })
+     }
+     
+      createDisplay(sortArr);
+      
+  }
+ }
